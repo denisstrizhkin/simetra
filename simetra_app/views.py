601b6fb1f088ef61cb52575cbe3906df4ee72b75
denis <strdenis02@gmail.com>
2022-03-08 22:09:56 +0300

    bosses display with django

--- simetra_app/views.py
+++ simetra_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import Boss, Employee
 
 
 def main_page(request):
-    return render(request, 'simetra_app/main.html')
+    context = {'bosses_list': Boss.objects.all(),
+               'employees_list': Employee.objects.all()}
+    return render(request, 'simetra_app/main.html', context)
 
 
 def methodology_page(request):
