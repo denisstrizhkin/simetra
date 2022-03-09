c470d16a05ab8cc32b7e0f2fd29559b76c99bff4
denis <strdenis02@gmail.com>
2022-03-09 15:32:28 +0300

    added employess images

--- simetra_app/models.py
+++ simetra_app/models.py
@@ -1,23 +1,41 @@
 from django.db import models
+from pathlib import Path
+import uuid
+
+
+def get_uuid4_filename(filename):
+    suffix = Path(filename).suffix
+    basename = Path(str(uuid.uuid4()))
+    return basename.with_suffix(suffix)
 
 
 class Boss(models.Model):
     def __str__(self):
         return self.name
 
+    def get_upload_path(self, filename):
+        return Path('bosses', get_uuid4_filename(filename))
+
     name = models.CharField(max_length=100)
     position = models.CharField(max_length=200)
     quote = models.TextField()
-    image = models.ImageField(default='default-boss.jpg')
+    image = models.ImageField(
+        default='default-boss.jpg',
+        upload_to=get_upload_path)
 
 
 class Employee(models.Model):
     def __str__(self):
         return self.name
 
+    def get_upload_path(self, filename):
+        return Path('employees', get_uuid4_filename(filename))
+
     name = models.CharField(max_length=100)
     position = models.CharField(max_length=200)
-    image = models.ImageField(default='default-employee.jpg')
+    image = models.ImageField(
+        default='default-employee.jpg',
+        upload_to=get_upload_path)
 
 
 class City(models.Model):
