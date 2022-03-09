03a5bb0c9388010819d2df5f7be23b79fd47045b
denis <strdenis02@gmail.com>
2022-03-09 12:00:13 +0300

    added images for bosses

--- simetra/settings.py
+++ simetra/settings.py
@@ -123,12 +123,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'images/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_ROOT = BASE_DIR / 'simetra_app/static/images'
 
 STATICFILES_DIRS = [
     (BASE_DIR / 'simetra_app/static')
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- simetra_app/urls.py
+++ simetra_app/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 
 from simetra_app import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 app_name = 'simetra_app'
 
 
@@ -10,3 +13,7 @@ urlpatterns = [
     path('', views.main_page, name='main'),
     path('methodogoly/', views.methodology_page, name='methodology'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
