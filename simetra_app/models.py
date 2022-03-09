b727c9fbb27eeccdce1e7074cb607916124b77c5
denis <strdenis02@gmail.com>
2022-03-09 11:39:15 +0300

    added 2 bosses entries to db

--- simetra_app/models.py
+++ simetra_app/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 class Boss(models.Model):
+    def __str__(self):
+        return self.name
+
     name = models.CharField(max_length=100)
     position = models.CharField(max_length=200)
     quote = models.TextField()
@@ -9,10 +12,16 @@ class Boss(models.Model):
 
 
 class Employee(models.Model):
+    def __str__(self):
+        return self.name
+
     name = models.CharField(max_length=100)
     position = models.CharField(max_length=200)
     image = models.ImageField(default='default-employee.jpg')
 
 
 class City(models.Model):
+    def __str__(self):
+        return self.name
+
     name = models.CharField(max_length=100)
