fd39e8b22914988348bbb8e43b05109f50178cde
AnoMorCH <anomorch@gmail.com>
2022-03-09 11:22:42 +0300

    Cleared Code

--- simetra_app/views.py
+++ simetra_app/views.py
@@ -3,8 +3,10 @@ from .models import Boss, Employee
 
 
 def main_page(request):
-    context = {'bosses_list': Boss.objects.all(),
-               'employees_list': Employee.objects.all()}
+    context = {
+        'bosses_list': Boss.objects.all(),
+        'employees_list': Employee.objects.all()
+    }
     return render(request, 'simetra_app/main.html', context)
 
 
