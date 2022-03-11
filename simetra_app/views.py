3f4f53eb758cf8100e426bcfe5e6493a88ddbbe3
denis <strdenis02@gmail.com>
2022-03-11 12:45:59 +0300

    number of cities

--- simetra_app/views.py
+++ simetra_app/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from .models import Boss, Employee
+from .models import Boss, Employee, City
 
 
 def main_page(request):
     context = {
         'bosses_list': Boss.objects.all(),
-        'employees_list': Employee.objects.all()
+        'employees_list': Employee.objects.all(),
+        'number_of_cities': City.objects.all().count()
     }
     return render(request, 'simetra_app/main.html', context)
 
