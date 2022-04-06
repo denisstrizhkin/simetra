506f4b963f19422f985adc89a18939b3c530cced
klekkwedge <antonov.andrey.10101@gmail.com>
2022-04-06 14:13:15 +0300

    feat: change visual prototype for spatial section

--- simetra_app/static/simetra_app/city_page/scripts/blank-script.js
+++ simetra_app/static/simetra_app/city_page/scripts/blank-script.js
@@ -20,6 +20,13 @@ function findCity() {
 }
 const cityObj = findCity();
 
+
+
+
+
+
+console.log(Object.keys(cityObj));
+
 function generateDatas(objectField, start, end) {
   let data = [];
   for (let i = start; i < end; i++) {
@@ -28,7 +35,7 @@ function generateDatas(objectField, start, end) {
   return data;
 }
 
-function createNewChart(start, end) {
+function createNewPolarArea(start, end) {
   const data = {
     labels: generateDatas(Object.keys(cityObj), start, end),
     datasets: [
@@ -65,10 +72,71 @@ function createNewChart(start, end) {
   return config;
 }
 
-new Chart(document.getElementById("myChart"), createNewChart(3, 8));
-new Chart(document.getElementById("myChart-2"), createNewChart(8, 13));
-new Chart(document.getElementById("myChart-3"), createNewChart(13, 18));
-new Chart(document.getElementById("myChart-4"), createNewChart(18, 23));
-new Chart(document.getElementById("myChart-5"), createNewChart(23, 28));
-new Chart(document.getElementById("myChart-6"), createNewChart(28, 33));
-new Chart(document.getElementById("myChart-7"), createNewChart(33, 38));
+new Chart(document.getElementById("myChart"), createNewPolarArea(3, 8));
+
+
+
+
+function createNewBar(start, end) {
+  // const data = {
+  //   labels: generateDatas(Object.keys(cityObj), start, end),
+  //   datasets: [
+  //     {
+  //       label: "Dataset 1",
+  //       data: generateDatas(Object.values(cityObj), start, end),
+  //       backgroundColor: [
+  //         "rgba(255, 99, 132,1)",
+  //         "rgba(54, 162, 235,1)",
+  //         "rgba(255, 206, 86,1)",
+  //         "rgba(75, 192, 192,1)",
+  //         "rgba(153, 102, 255,1)",
+  //       ],
+  //     },
+  //   ],
+  // };
+
+  const data = {
+    labels: generateDatas(Object.keys(cityObj), start, end),
+    datasets: [
+      {
+        label: 'Пространственные характеристики',
+        data: generateDatas(Object.values(cityObj), start, end),
+        backgroundColor: [
+                  "rgba(255, 99, 132,1)",
+                  "rgba(54, 162, 235,1)",
+                  "rgba(255, 206, 86,1)",
+                  "rgba(75, 192, 192,1)",
+                  "rgba(153, 102, 255,1)",
+                ],
+        stack: 'combined',
+        type: 'bar'
+      }
+    ]
+  };
+
+  const config = {
+    type: 'bar',
+    data: data,
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top',
+        },
+        title: {
+          display: true,
+          text: 'Chart.js Bar Chart'
+        }
+      }
+    },
+  };
+  return config;
+}
+
+
+new Chart(document.getElementById("myChart-2"), createNewBar(8, 13));
+new Chart(document.getElementById("myChart-3"), createNewBar(13, 18));
+new Chart(document.getElementById("myChart-4"), createNewBar(18, 23));
+new Chart(document.getElementById("myChart-5"), createNewBar(23, 28));
+new Chart(document.getElementById("myChart-6"), createNewBar(28, 33));
+new Chart(document.getElementById("myChart-7"), createNewBar(33, 38));
