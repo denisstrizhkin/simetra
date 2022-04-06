bbbb86c4816f16ba740e537e10ee391c5686e834
klekkwedge <antonov.andrey.10101@gmail.com>
2022-04-06 12:55:58 +0300

    feat: add visual prototype for spatial section

--- simetra_app/static/simetra_app/city_page/scripts/blank-script.js
+++ simetra_app/static/simetra_app/city_page/scripts/blank-script.js
@@ -20,53 +20,55 @@ function findCity() {
 }
 const cityObj = findCity();
 
-// function generateData(index) {
-//   return {
-//     category: Object.keys(cityObj)[index],
-//     value: Object.values(cityObj)[index],
-//   };
-// }
-
-function generateDatas(objectField) {
+function generateDatas(objectField, start, end) {
   let data = [];
-  for (let i = 3; i < 8; i++) {
+  for (let i = start; i < end; i++) {
     data.push(objectField[i]);
   }
   return data;
 }
 
-const data = {
-  labels: generateDatas(Object.keys(cityObj)),
-  datasets: [
-    {
-      label: "Dataset 1",
-      data: generateDatas(Object.values(cityObj)),
-      backgroundColor: [
-        "rgba(255, 99, 132,1)",
-        "rgba(54, 162, 235,1)",
-        "rgba(255, 206, 86,1)",
-        "rgba(75, 192, 192,1)",
-        "rgba(153, 102, 255,1)",
-      ],
-    },
-  ],
-};
-
-const config = {
-  type: "polarArea",
-  data: data,
-  options: {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: "top",
+function createNewChart(start, end) {
+  const data = {
+    labels: generateDatas(Object.keys(cityObj), start, end),
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: generateDatas(Object.values(cityObj), start, end),
+        backgroundColor: [
+          "rgba(255, 99, 132,1)",
+          "rgba(54, 162, 235,1)",
+          "rgba(255, 206, 86,1)",
+          "rgba(75, 192, 192,1)",
+          "rgba(153, 102, 255,1)",
+        ],
       },
-      title: {
-        display: true,
-        text: "Chart.js Polar Area Chart",
+    ],
+  };
+
+  const config = {
+    type: "polarArea",
+    data: data,
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: "top",
+        },
+        title: {
+          display: true,
+          text: "Chart.js Polar Area Chart",
+        },
       },
     },
-  },
-};
+  };
+  return config;
+}
 
-const myChart = new Chart(document.getElementById("myChart"), config);
+new Chart(document.getElementById("myChart"), createNewChart(3, 8));
+new Chart(document.getElementById("myChart-2"), createNewChart(8, 13));
+new Chart(document.getElementById("myChart-3"), createNewChart(13, 18));
+new Chart(document.getElementById("myChart-4"), createNewChart(18, 23));
+new Chart(document.getElementById("myChart-5"), createNewChart(23, 28));
+new Chart(document.getElementById("myChart-6"), createNewChart(28, 33));
+new Chart(document.getElementById("myChart-7"), createNewChart(33, 38));
