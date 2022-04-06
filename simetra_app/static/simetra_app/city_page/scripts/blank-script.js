90ad97bb2fb7c28261d5318a09552179d8057457
klekkwedge <antonov.andrey.10101@gmail.com>
2022-04-06 12:09:43 +0300

    feat: add visualization for quality groups

--- simetra_app/static/simetra_app/city_page/scripts/blank-script.js
+++ simetra_app/static/simetra_app/city_page/scripts/blank-script.js
@@ -19,3 +19,54 @@ function findCity() {
   }
 }
 const cityObj = findCity();
+
+// function generateData(index) {
+//   return {
+//     category: Object.keys(cityObj)[index],
+//     value: Object.values(cityObj)[index],
+//   };
+// }
+
+function generateDatas(objectField) {
+  let data = [];
+  for (let i = 3; i < 8; i++) {
+    data.push(objectField[i]);
+  }
+  return data;
+}
+
+const data = {
+  labels: generateDatas(Object.keys(cityObj)),
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: generateDatas(Object.values(cityObj)),
+      backgroundColor: [
+        "rgba(255, 99, 132,1)",
+        "rgba(54, 162, 235,1)",
+        "rgba(255, 206, 86,1)",
+        "rgba(75, 192, 192,1)",
+        "rgba(153, 102, 255,1)",
+      ],
+    },
+  ],
+};
+
+const config = {
+  type: "polarArea",
+  data: data,
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Chart.js Polar Area Chart",
+      },
+    },
+  },
+};
+
+const myChart = new Chart(document.getElementById("myChart"), config);
