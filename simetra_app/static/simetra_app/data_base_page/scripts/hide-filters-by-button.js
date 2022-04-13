eee209b5d94623e2ae621e2ba98046280d7a45cd
AnoMorCH <anomorch@gmail.com>
2022-04-13 14:34:31 +0300

    feat: add 'фильтры' button

---
+++ simetra_app/static/simetra_app/data_base_page/scripts/hide-filters-by-button.js
@@ -0,0 +1,74 @@
+function getWidth() {
+    return Math.max(
+        document.body.scrollWidth,
+        document.documentElement.scrollWidth,
+        document.body.offsetWidth,
+        document.documentElement.offsetWidth,
+        document.documentElement.clientWidth
+    );
+}
+
+function isAdaptive() {
+    return getWidth() <= 1080;
+}
+
+function showOrHideFilterByButton() {
+    const filter = document.getElementsByName('filters')[0];
+    let isFilterHidden = filter.classList.contains('_hide-element');
+
+    if (isFilterHidden) {
+        filter.classList.remove('_hide-element');
+    } else {
+        filter.classList.add('_hide-element');
+    }
+}
+
+function showOrHideFilterByApative() {
+    const filter = document.getElementsByName('filters')[0];
+    const isFilterHidden = filter.classList.contains('_hide-element');
+
+    if (isAdaptive() && !isFilterHidden) {
+        filter.classList.add('_hide-element');
+    }
+
+    if (!isAdaptive() && isFilterHidden) {
+        filter.classList.remove('_hide-element');
+    }
+}
+
+function showOrHideFilterButtonByAdaptive() {
+    const button = document.getElementsByName('filter-button')[0];
+    const isButtonHidden = button.classList.contains('_hide-element');
+
+    if (isAdaptive() && isButtonHidden) {
+        button.classList.remove('_hide-element');
+    }
+
+    if (!isAdaptive() && !isButtonHidden) {
+        button.classList.add('_hide-element');
+    }
+}
+
+if (window.attachEvent) {
+    window.attachEvent('onresize', () => {
+        showOrHideFilterButtonByAdaptive();
+        showOrHideFilterByApative();
+    });
+}
+else if (window.addEventListener) {
+    window.addEventListener('resize', () => {
+        showOrHideFilterButtonByAdaptive();
+        showOrHideFilterByApative();
+    }, true);
+}
+
+const filterButton = document.getElementsByName("filter-button")[0];
+
+filterButton.addEventListener('click', () => {
+    showOrHideFilterByButton(filterButton);
+}, false);
+
+showOrHideFilterButtonByAdaptive();
+showOrHideFilterByApative();
+
+console.log(getWidth());
