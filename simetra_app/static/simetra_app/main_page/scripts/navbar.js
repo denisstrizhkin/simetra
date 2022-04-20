81ddc1adef60f7c32f5414fecd2be52551b2acbd
Anton Morozov <78485324+AnoMorCH@users.noreply.github.com>
2022-04-20 10:32:11 +0300

    Revert "new main page"

--- simetra_app/static/simetra_app/main_page/scripts/navbar.js
+++ simetra_app/static/simetra_app/main_page/scripts/navbar.js
@@ -1,3 +1,3 @@
 $(document).scroll(function (e) {
-    ($(window).scrollTop() > 690 && $(window).scrollTop() < 2412 || $(window).scrollTop() > 3027 && $(window).scrollTop() < 4774) ? $('.header').addClass('header__color') : $('.header').removeClass('header__color');
+    ($(window).scrollTop() > 950 && $(window).scrollTop() < 3500 || $(window).scrollTop() > 3880 && $(window).scrollTop() < 5550) ? $('.header').addClass('header__color') : $('.header').removeClass('header__color');
 });
