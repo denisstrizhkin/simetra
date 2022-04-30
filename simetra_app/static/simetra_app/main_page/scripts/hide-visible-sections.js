dba86c2d0d814dfc072ebf354df1e880ff088e66
daria-vereshak <dariavereshak02@gmail.com>
2022-04-30 14:51:50 +0300

    add section methodology & fix some space on main

--- simetra_app/static/simetra_app/main_page/scripts/hide-visible-sections.js
+++ simetra_app/static/simetra_app/main_page/scripts/hide-visible-sections.js
@@ -5,40 +5,43 @@ function come(elem, dif) {
 }
 
 $(document).scroll(function (e) {
-  if (come('.comments', 100)) { 
+  if (come('.comments', 100)) {
     $('.comments').addClass('visible');
   }
-  if (come('.about', 100))  {
+  if (come('.about', 100)) {
     $('.about').addClass('visible');
   }
-  if (come('.about__text-image', 30))  {
+  if (come('.about__text-image', 30)) {
     $('.about__text-image').addClass('visible');
   }
-  if (come('.about__image', 150))  {
+  if (come('.about__image', 150)) {
     $('.about__image').addClass('visible');
   }
-  if (come('.about__youtube-video', 100))  {
+  if (come('.about__youtube-video', 100)) {
     $('.about__youtube-video').addClass('visible');
   }
-  if (come('.about__text-video', 30))  {
+  if (come('.about__text-video', 30)) {
     $('.about__text-video').addClass('visible');
   }
-  if (come('.map', 100))  {
+  if (come('.methodology', 50)) {
+    $('.methodology').addClass('visible');
+  }
+  if (come('.map', 100)) {
     $('.map').addClass('visible');
   }
-  if (come('.city--first', 110))  {
+  if (come('.city--first', 110)) {
     $('.city--first').addClass('visible');
   }
-  if (come('.city--second', 200))  {
+  if (come('.city--second', 200)) {
     $('.city--second').addClass('visible');
   }
-  if (come('.city--third', 90))  {
+  if (come('.city--third', 90)) {
     $('.city--third').addClass('visible');
   }
-  if (come('.city--forth', 210))  {
+  if (come('.city--forth', 210)) {
     $('.city--forth').addClass('visible');
   }
-  if (come('.employee__image', 30))  {
+  if (come('.employee__image', 30)) {
     $('.employee__image').addClass('visible');
   }
 
